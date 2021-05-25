2670e30ac0d384781b6aa4d15540768f54bc95cb
Malik <malikoabdulaziz@gmail.com>
2021-05-25 09:49:55 +0100

    demo-deploy

--- src/components/Interest/Interest.js
+++ src/components/Interest/Interest.js
@@ -15,8 +15,12 @@ function Interest() {
           <div className="interest">
             <h2>Interests</h2>
             <p>
-              When I am not writing code, I like to watch a good Movie or some
-              Anime. I like to read too, which I do not do enough of these days.
+              When I am not writing code, I like to watch a good movie or some
+              anime. I like to read too, which I do not do enough of these days.
+            </p>
+            <p>
+              I also like to learn about new stuff, and podcasts are my favorite
+              medium of consuming new information.
             </p>
             <p>
               I'm currently Working on a flappy bird clone with vanilla JS check
