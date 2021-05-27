5ba9c9cdeaf0bbd67272615e1b6d502a48c3b88a
Malik <malikoabdulaziz@gmail.com>
2021-05-27 10:33:54 +0100

    a new face lift

--- src/components/Home/HeroSection.js
+++ src/components/Home/HeroSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../Nav/Footer";
 import Header from "../Nav/Header";
-import avatar from "../../assets/avatar.jpg";
+import avatar from "../../assets/avatarr.jpg";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logoo.svg";
 import Socials from "../Socials/Socials";
@@ -18,7 +18,7 @@ function HeroSection() {
             <div className="image">
               <img src={avatar} alt="avatar" />
             </div>
-            <h1>Hi, I am Malik, Fullstack Developer.</h1>
+            <h1>Hi, I am Malik, and I'm a Fullstack Developer.</h1>
             <p>
               I write React on the Frontend, and NodeJS on the backend. Give me
               a design and I will bring it to life.
