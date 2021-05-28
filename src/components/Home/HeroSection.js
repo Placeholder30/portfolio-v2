701fa8c9d320fe65c5bb3e252229e038b16dd481
Malik <malikoabdulaziz@gmail.com>
2021-05-28 16:09:24 +0100

    remove home nav

--- src/components/Home/HeroSection.js
+++ src/components/Home/HeroSection.js
@@ -3,23 +3,23 @@ import Footer from "../Nav/Footer";
 import Header from "../Nav/Header";
 import avatar from "../../assets/avatarr.jpg";
 import Socials from "../Socials/Socials";
-import HomeIcon from "../HomeIcon";
 function HeroSection() {
   return (
     <>
       <Header />
       <div className="container">
-        <HomeIcon />
         <div className="intro-container">
           <div className="content">
             <div className="image">
               <img src={avatar} alt="avatar" />
             </div>
-            <h1>Hi, I am Malik, and I'm a Fullstack Developer.</h1>
-            <p>
-              I write React on the Frontend, and NodeJS on the backend. Give me
-              a design and I will bring it to life.
-            </p>
+            <div className="text">
+              <h1>Hi, I am Malik, and I'm a Fullstack Developer.</h1>
+              <p>
+                I write React on the Frontend, and NodeJS on the backend. Give
+                me a design and I will bring it to life.
+              </p>
+            </div>
           </div>
         </div>
         <Socials />
