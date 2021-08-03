6cfee8cb191f4f435f679e72c1f1d4727922ddc6
Malik. A <54292102+Placeholder30@users.noreply.github.com>
2021-08-03 09:07:54 +0100

    Update Project.js

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -55,7 +55,7 @@ The user can create an account, login and place an order. The
 user can also view all previous orders`;
 const lekkiStoreStack = ["React", "Express", "Postgres", "Styled Components"];
 
-const trimmerSummary = ` A url shortener app built with React Express and Redis, Enter you long url and get a shortened one returned`;
+const trimmerSummary = ` A url shortener app built with React Express and Redis. Enter you long url and get a shortened one returned`;
 const trimmerStack = ["React", "NodeJS", "Redis"];
 
 const socialMediaFeedSummary = `This is a social media feed, one of the first projects I made. I
